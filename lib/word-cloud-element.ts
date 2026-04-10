QuentinRoy/word-cloud

commit 305159f1c645487050bd3a621763eb134ba2b18c

fix chrome viewport bug (#28)
* fix chrome viewport bug

Fixes #22

* test: update visual regression screenshots

Co-authored-by: QuentinRoy <728801+QuentinRoy@users.noreply.github.com>

---------

Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

lib/word-cloud-element.css
@@ -119,11 +119,12 @@
 
 .word-cloud .word {
 	position: absolute;
-	top: 0;
-	left: 0;
 	z-index: 3;
-	translate: -50% -50%;
-	will-change: transform;
+	/* Fixes what I believe is a bug in Chrome: viewport is getting enlarged
+	in case of rotations, adding scrollbars to view non-existing content.
+	c.f https://github.com/QuentinRoy/word-cloud/issues/22 */
+	overflow: hidden;
+	will-change: top, left, rotate;
 }
 
 input[type="text"] {

lib/word-cloud-element.ts
@@ -407,7 +407,7 @@ export class HTMLWordCloudElement extends WithAttributeProps(HTMLElement, {
 				}),
 			)
 		})
-		element.style.transform = this.#getWordTransform(entry)
+		Object.assign(element.style, this.#getWordStyle(entry))
 		if (velocity) Body.setVelocity(body, velocity)
 		Composite.add(this.#engine.world, body)
 		this.#wordEntries.set(id, entry)
@@ -927,7 +927,7 @@ export class HTMLWordCloudElement extends WithAttributeProps(HTMLElement, {
 
 	#updateWordPositions() {
 		for (let entry of this.#wordEntries.values()) {
-			entry.element.style.transform = this.#getWordTransform(entry)
+			Object.assign(entry.element.style, this.#getWordStyle(entry))
 		}
 	}
 
@@ -955,18 +955,27 @@ export class HTMLWordCloudElement extends WithAttributeProps(HTMLElement, {
 		}
 	}
 
-	#getWordTransform({ body }: InternalWordEntry) {
+	#getWordStyle({ body, bodySize: { width, height } }: InternalWordEntry) {
+		let result: Partial<CSSStyleDeclaration> = {}
 		let angle = toPrecision(body.angle, ROTATE_PRECISION)
-		let translateX = toPrecision(body.position.x, TRANSLATE_PRECISION)
-		let translateY = toPrecision(body.position.y, TRANSLATE_PRECISION)
-		let transform = ""
-		if (translateX !== 0 || translateY !== 0) {
-			transform += `translate(${translateX}px, ${translateY}px)`
+		let translateX = toPrecision(
+			body.position.x - width / 2,
+			TRANSLATE_PRECISION,
+		)
+		let translateY = toPrecision(
+			body.position.y - height / 2,
+			TRANSLATE_PRECISION,
+		)
+		if (translateX !== 0) {
+			result.left = `${translateX}px`
+		}
+		if (translateY !== 0) {
+			result.top = `${translateY}px`
 		}
 		if (angle !== 0) {
-			transform += ` rotate(${angle}rad)`
+			result.rotate = `${angle}rad`
 		}
-		return transform
+		return result
 	}
 
 	#updateWordsActionFromWordAction() {

commit f2375e7cf1cb22d51987063e1b410b9986e9ed60

fix word's transformation (#31)
* fix words style calculation

Fix a regression introduced by #28, and improve speed by reverting back to the use of transform instead of left, right, and rotate.

* test: update visual regression screenshots

Co-authored-by: QuentinRoy <728801+QuentinRoy@users.noreply.github.com>

---------

Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

lib/word-cloud-element.css
@@ -119,8 +119,10 @@
 
 .word-cloud .word {
 	position: absolute;
+	top: 0;
+	left: 0;
 	z-index: 3;
-	will-change: top, left, rotate;
+	will-change: transform;
 }
 
 input[type="text"] {

lib/word-cloud-element.ts
@@ -50,9 +50,9 @@ const MAX_RANDOM_VELOCITY = 40
 const PADDING = 0
 const INPUT_VOLUME_MIN_SIZE = 1
 const TRANSLATE_PRECISION = 1
-const ROTATE_PRECISION = 4
+const ROTATE_PRECISION = 3
 const ANGULAR_REST_ANGLE = 0
-const ANGULAR_REST_ANGLE_EPSILON = 0.005
+const ANGULAR_REST_ANGLE_EPSILON = 0.001
 const ANGULAR_SPRING_TORQUE_STIFFNESS = 0.4
 const ANGULAR_DAMPING_COEFFICIENT = 0.2
 const ANGULAR_SPRING_WIDTH_REFERENCE = 150
@@ -407,7 +407,7 @@ export class HTMLWordCloudElement extends WithAttributeProps(HTMLElement, {
 				}),
 			)
 		})
-		Object.assign(element.style, this.#getWordStyle(entry))
+		element.style.transform = this.#getWordTransform(entry)
 		if (velocity) Body.setVelocity(body, velocity)
 		Composite.add(this.#engine.world, body)
 		this.#wordEntries.set(id, entry)
@@ -927,7 +927,7 @@ export class HTMLWordCloudElement extends WithAttributeProps(HTMLElement, {
 
 	#updateWordPositions() {
 		for (let entry of this.#wordEntries.values()) {
-			Object.assign(entry.element.style, this.#getWordStyle(entry))
+			entry.element.style.transform = this.#getWordTransform(entry)
 		}
 	}
 
@@ -955,8 +955,10 @@ export class HTMLWordCloudElement extends WithAttributeProps(HTMLElement, {
 		}
 	}
 
-	#getWordStyle({ body, bodySize: { width, height } }: InternalWordEntry) {
-		let result: Partial<CSSStyleDeclaration> = {}
+	#getWordTransform({
+		body,
+		bodySize: { width, height },
+	}: InternalWordEntry): string {
 		let angle = toPrecision(body.angle, ROTATE_PRECISION)
 		let translateX = toPrecision(
 			body.position.x - width / 2,
@@ -966,16 +968,9 @@ export class HTMLWordCloudElement extends WithAttributeProps(HTMLElement, {
 			body.position.y - height / 2,
 			TRANSLATE_PRECISION,
 		)
-		if (translateX !== 0) {
-			result.left = `${translateX}px`
-		}
-		if (translateY !== 0) {
-			result.top = `${translateY}px`
-		}
-		if (angle !== 0) {
-			result.rotate = `${angle}rad`
-		}
-		return result
+		return angle !== 0
+			? `translate(${translateX}px, ${translateY}px) rotate(${angle}rad)`
+			: `translate(${translateX}px, ${translateY}px)`
 	}
 
 	#updateWordsActionFromWordAction() {