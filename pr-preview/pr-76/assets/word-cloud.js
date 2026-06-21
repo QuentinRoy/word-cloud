9991dd80d73a1acc422c4158dcc1e8d1eb946c41
QuentinRoy <QuentinRoy@users.noreply.github.com>
2026-06-21 11:36:18 +0000

    Deploy preview for PR 76 🛫

--- pr-preview/pr-76/assets/word-cloud.js
+++ pr-preview/pr-76/assets/word-cloud.js
@@ -1197,4 +1197,4 @@ var oe = class i extends e(HTMLElement, {
 //#endregion
 export { oe as HTMLWordCloudElement, p as PhysicsPauseEvent, d as WordActionChangeEvent, s as WordAddEvent, l as WordChangeEvent, c as WordCheckEvent, u as WordDeleteEvent, N as WordHandle, f as WordInputToggleEvent, se as version };
 
-//# sourceMappingURL=index.js.map
+//# sourceMappingURL=word-cloud.js.map
